b6e96b4af244308e23cffd041723bde142a5dbf3
김률아 <hobby519@naver.com>
2025-05-05 12:58:21 +0900

    feat: 공통 응답코드 및 예외 핸들러 및 외 (다른 서버 용)

--- src/main/java/com/library/common/exception/GlobalException.java
+++ src/main/java/com/library/common/exception/GlobalException.java
@@ -1,11 +1,11 @@
 package com.library.common.exception;
 
-import com.library.common.code.ErrorCode;
+import com.library.common.code.StatusCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public class GlobalException extends RuntimeException{
-    private final ErrorCode errorCode;
+    private final StatusCode errorCode;
 }
